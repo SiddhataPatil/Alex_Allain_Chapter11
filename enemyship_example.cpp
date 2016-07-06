06d4a3760b8a02dcb3bcd4454941d173eddf4e0a
Sid <siddhata.s.patil@gmail.com>
2016-07-06 10:25:43 -0700

    Update enemyship_example.cpp

--- enemyship_example.cpp
+++ enemyship_example.cpp
@@ -2,8 +2,8 @@
  ==============================================================================================================================
  Name        : enemyship_example.cpp
  Author      : Siddhata Patil
- Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
- Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
  Question    : Upgrade ship Power
  ==============================================================================================================================
  */
