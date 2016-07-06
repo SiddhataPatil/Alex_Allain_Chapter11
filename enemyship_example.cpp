c803fc25bd7f86272a2d066789c8c1ff47ebd808
Sid <siddhata.s.patil@gmail.com>
2016-07-05 18:35:25 -0700

    Create enemyship_example.cpp

---
+++ enemyship_example.cpp
@@ -0,0 +1,54 @@
+/*
+ ==============================================================================================================================
+ Name        : enemyship_example.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Upgrade ship Power
+ ==============================================================================================================================
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct Enemyspaceship
+{
+int x;
+int y;
+int power;
+};
+
+Enemyspaceship getNewEnemy()
+{
+Enemyspaceship enemy;
+enemy.x=0;
+enemy.y=0;
+enemy.power=40;
+return enemy;
+}
+
+Enemyspaceship upgradeWeapons(Enemyspaceship weapon)
+{
+	weapon.power +=30;
+	return weapon;
+}
+
+int main()
+{
+	clock_t begin = clock();
+
+Enemyspaceship fd1;
+Enemyspaceship fd2;
+fd1 = getNewEnemy();
+fd2 = upgradeWeapons(fd1);
+cout<< "Power = "<<fd1.power<<endl;
+cout<<"Power = " << fd2.power<<endl;
+
+	clock_t end = clock();
+	double elapsed_secs = double(end-begin)/CLOCKS_PER_SEC;
+	cout<<elapsed_secs<<" sec"<<endl;
+	system ("pause");
+	return 0;
+}
