766036abf32d5ced816a01b501a2131d145d83df
Sid <siddhata.s.patil@gmail.com>
2016-07-05 18:33:04 -0700

    Create name_skills_struct.cpp

---
+++ name_skills_struct.cpp
@@ -0,0 +1,58 @@
+/*
+ ==============================================================================================================================
+ Name        : name_skills_struct.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Simple struct skeleton
+ ==============================================================================================================================
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct Info
+{
+	string name;
+	int skills;
+};
+int addskills(Info pl[], int si)
+{
+int sum=0;
+for (int i=0;i<3;i++)
+{
+	sum+=pl[i].skills;
+}
+	return sum;
+}
+void disp (Info players[], int size)
+{
+	for (int i=0; i<3 ; i++)
+	{cout << players[i].name<<" has " << players[i].skills<< " skills."<<endl;
+	}
+}
+
+int main()
+{
+	clock_t begin = clock();
+
+Info players[3];
+for (int i =0; i<3; i++)
+{
+	cout<<"Please enter your name"<<endl;
+	cin>>players[i].name;
+	cout<<"Enter no. of skills" << endl;
+	cin>> players[i].skills;
+}
+disp(players, 3);
+int total = addskills(players,3);
+cout<< total <<endl;
+
+	clock_t end = clock();
+	double elapsed_secs = double(end-begin)/CLOCKS_PER_SEC;
+	cout<<elapsed_secs<<" sec"<<endl;
+	system ("pause");
+	return 0;
+}
